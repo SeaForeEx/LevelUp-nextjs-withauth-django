829147be98375b35420dfdcd55f8905e2e13b554
Charles Bridgers IV (he/him) <charlesbridgersiv@gmail.com>
2023-05-31 07:22:30 -0500

    added some comments

--- components/game/GameCard.js
+++ components/game/GameCard.js
@@ -12,24 +12,28 @@ const GameCard = ({
   skillLevel,
   onUpdate,
 }) => {
+  // Define a function to handle deleting the game
   const deleteThisGame = () => {
+    // Display a confirmation dialog to confirm the deletion
     if (window.confirm('Delete Game?')) {
-      deleteGame(id).then(() => onUpdate());
+      // Call the deleteGame function to delete the game by its ID
+      deleteGame(id).then(() => onUpdate()); // Call the onUpdate function to update the game list after deletion
     }
   };
-  const router = useRouter();
+
+  const router = useRouter(); // Get the router instance from Next.js
 
   return (
     <Card className="text-center">
-      <Card.Header>{title}</Card.Header>
+      <Card.Header>{title}</Card.Header> {/* Display the game title */}
       <Card.Body>
-        <Card.Title>By: {maker}</Card.Title>
-        <Card.Text>{numberOfPlayers} players needed</Card.Text>
+        <Card.Title>By: {maker}</Card.Title> {/* Display the game maker */}
+        <Card.Text>{numberOfPlayers} players needed</Card.Text> {/* Display the number of players needed */}
       </Card.Body>
-      <Card.Footer className="text-muted">Skill Level: {skillLevel}</Card.Footer>
+      <Card.Footer className="text-muted">Skill Level: {skillLevel}</Card.Footer> {/* Display the game's skill level */}
       <Button
         onClick={() => {
-          router.push(`/games/edit/${id}`);
+          router.push(`/games/edit/${id}`); // Navigate to the game edit page when clicked
         }}
       >
         Edit Game
@@ -42,12 +46,12 @@ const GameCard = ({
 };
 
 GameCard.propTypes = {
-  id: PropTypes.number.isRequired,
-  title: PropTypes.string.isRequired,
-  maker: PropTypes.string.isRequired,
-  numberOfPlayers: PropTypes.number.isRequired,
-  skillLevel: PropTypes.number.isRequired,
-  onUpdate: PropTypes.func.isRequired,
+  id: PropTypes.number.isRequired, // ID of the game (required)
+  title: PropTypes.string.isRequired, // Title of the game (required)
+  maker: PropTypes.string.isRequired, // Maker of the game (required)
+  numberOfPlayers: PropTypes.number.isRequired, // Number of players needed for the game (required)
+  skillLevel: PropTypes.number.isRequired, // Skill level of the game (required)
+  onUpdate: PropTypes.func.isRequired, // Callback function to update the game list (required)
 };
 
 export default GameCard;
